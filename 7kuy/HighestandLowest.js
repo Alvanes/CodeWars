adcf1aabec0fdd07354a88ac728b31fb5153ca16
Alvanes <yan.spbk@mail.ru>
2021-11-18 00:25:22 +0300

    CodeWarsTask

--- 7kuy/HighestandLowest.js
+++ 7kuy/HighestandLowest.js
@@ -1,3 +1,14 @@
-function multiply(a, b){
-  return a * b
+function highAndLow(numbers){
+   var maxmin = "";
+        var max = 0;
+        var min = 0;
+        const arr = numbers.split(" ").map(Number);
+        min = arr[0];
+        max = min;
+        for (var i = 1; i < arr.length; ++i) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+        maxmin = max.toString() + " " + min.toString();
+        return maxmin;
 }
