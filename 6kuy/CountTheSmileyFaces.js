43c006a7f2bdf8e8c783750715de03ebd4c647fb
Alvanes <yan.spbk@mail.ru>
2021-11-19 12:26:49 +0300

    CodeWarsTasks

---
+++ 6kuy/CountTheSmileyFaces.js
@@ -0,0 +1,18 @@
+function countSmileys(arr) {
+   let col = 0;
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i].length <2 || arr[i].length >3) {continue}
+    if (arr[i].length == 3) {
+      if (arr[i].includes(":")||arr[i].includes(";")) {
+        if (arr[i].includes("-")||arr[i].includes("~")) {
+            if (arr[i].includes(')')||arr[i].includes("D")) {  col++; }         
+        } 
+      } 
+    }
+    else 
+    if (arr[i].includes(":")||arr[i].includes(";")) { 
+            if (arr[i].includes(')')||arr[i].includes("D")) {  col++;  }
+       } 
+  }
+    return col;
+}
